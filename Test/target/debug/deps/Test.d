d1c522519786b5b7b6bdb17e228d829c2f940506
DefinetelyEzra <115805761+DefinetelyEzra@users.noreply.github.com>
2022-11-16 21:43:00 +0200

    Delete Test directory

--- Test/target/debug/deps/Test.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Foluso\Documents\O.AgunbiadeCSC101\Test\target\debug\deps\Test.exe: src\main.rs
-
-C:\Users\Foluso\Documents\O.AgunbiadeCSC101\Test\target\debug\deps\Test.d: src\main.rs
-
-src\main.rs:
